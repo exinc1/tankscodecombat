8de52961f6808e2ec47847ce9314f8a8e1c3b8ea
exinc1 <avivi.farkash@gmail.com>
2025-12-28 09:14:26 +0200

    random changes

--- app/src/main/java/com/example/tankscodecombat/bots/RandomBot1.java
+++
@@ -1,8 +0,0 @@
-package com.example.tankscodecombat.bots;
-public class RandomBot1 {
-    // input: direction as string
-    // output: action as string
-    public String run(int direction) {
-        return "MOVE_SLOW";
-    }
-}
